c5e604c3adfbd8fdefa63a84b0f2d33d8b860ec1
kaokei <kaokei.rst@gmail.com>
2021-12-10 14:07:21 +0800

    test(test): 增加测试用例InjectFailed

---
+++ tests/errors/INJECT_FAILED_2.spec.ts
@@ -0,0 +1,50 @@
+import 'reflect-metadata';
+import { Inject, Injector, Injectable, InjectFailedError } from '@/index';
+
+interface IA {
+  name: string;
+  id: number;
+  b: IB;
+}
+
+interface IB {
+  name: string;
+  id: number;
+}
+
+@Injectable()
+export class A {
+  public name = 'A';
+  public id = 1;
+
+  @Inject(Object)
+  public b!: IB;
+}
+
+@Injectable()
+export class B {
+  public name = 'B';
+  public id = 2;
+}
+
+describe('errors -> INJECT_FAILED: Property @Inject use Object', () => {
+  let injector: Injector;
+
+  beforeEach(() => {
+    injector = new Injector([A, B]);
+  });
+
+  test('injector.get(A) should throw InjectFailedError', async () => {
+    expect(() => {
+      injector.get(A);
+    }).toThrowError(InjectFailedError);
+  });
+
+  test('injector.get(B) should work correctly', async () => {
+    const b = injector.get(B);
+
+    expect(b).toBeInstanceOf(B);
+    expect(b.id).toBe(2);
+    expect(b.name).toBe('B');
+  });
+});
