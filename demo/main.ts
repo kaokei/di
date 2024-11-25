95b7381e0c33488db92732532510c52fbfab2cec
kaokei <kaokei.rst@gmail.com>
2024-11-25 20:51:53 +0800

    采用使用vite打包-计划API和inversely对齐

---
+++ demo/main.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+
+createApp(App).mount('#app');
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+/// <reference types="vitest/config" />
+
+// 参考：https://innei.in/posts/tech/write-a-universally-compatible-js-library-with-fully-types
+// 参考：https://arethetypeswrong.github.io/
+// 作为library是没有必要压缩的，除非需要输出umd格式给浏览器端使用
+// 注意reflect-metadata等库都是peerDependencies，不应该打包到当前库中
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    dts({
+      // rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+      beforeWriteFile: (filePath, content) => {
+        writeFileSync(filePath.replace('.d.ts', '.d.cts'), content);
+        return { filePath, content };
+      },
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'DI',
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['reflect-metadata'],
+      output: {
+        compact: true,
+      },
+    },
+  },
+  test: {
+    globals: true,
+    coverage: {
+      include: ['src/**/*.ts'],
+      reporter: ['text', 'lcov'],
+    },
+  },
+});
