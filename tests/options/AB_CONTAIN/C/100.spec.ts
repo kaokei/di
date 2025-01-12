a2ff4dd6935d83d974a08a215ca9b343f79b2459
kaokei <kaokei.rst@gmail.com>
2025-01-12 21:56:56 +0800

    优化100.spec.ts文件

--- tests/options/AB_CONTAIN/C/100.spec.ts
+++ tests/options/AB_CONTAIN/C/100.spec.ts
@@ -56,6 +56,21 @@ describe('00', () => {
     expect(hasOwn(child, B, b)).toBe(false);
     expect(hasOwn(parent, B, b)).toBe(true);
   });
+
+  test('parent.get(A) should throw ERROR_TOKEN_NOT_FOUND', async () => {
+    expect(() => {
+      parent.get(A);
+    }).toThrowError(TokenNotFoundError);
+  });
+
+  test('parent.get(B) should work correctly', async () => {
+    const b = parent.get(B);
+    expect(b).toBeInstanceOf(B);
+    expect(b.id).toBe(2);
+    expect(b.name).toBe('B');
+    expect(hasOwn(child, B, b)).toBe(false);
+    expect(hasOwn(parent, B, b)).toBe(true);
+  });
 });
 
 describe('01', () => {
@@ -82,6 +97,18 @@ describe('01', () => {
     expect(b.name).toBe('B');
     expect(hasOwn(child, B, b)).toBe(true);
     expect(hasOwn(parent, B, b)).toBe(false);
+  });
+
+  test('parent.get(A) should throw ERROR_TOKEN_NOT_FOUND', async () => {
+    expect(() => {
+      parent.get(A);
+    }).toThrowError(TokenNotFoundError);
+  });
+
+  test('parent.get(B) should throw ERROR_TOKEN_NOT_FOUND', async () => {
+    expect(() => {
+      parent.get(B);
+    }).toThrowError(TokenNotFoundError);
   });
 });
 
@@ -118,6 +145,21 @@ describe('10', () => {
     expect(hasOwn(child, B, b)).toBe(false);
     expect(hasOwn(parent, B, b)).toBe(true);
   });
+
+  test('parent.get(A) should throw ERROR_TOKEN_NOT_FOUND', async () => {
+    expect(() => {
+      parent.get(A);
+    }).toThrowError(TokenNotFoundError);
+  });
+
+  test('parent.get(B) should work correctly', async () => {
+    const b = parent.get(B);
+    expect(b).toBeInstanceOf(B);
+    expect(b.id).toBe(2);
+    expect(b.name).toBe('B');
+    expect(hasOwn(child, B, b)).toBe(false);
+    expect(hasOwn(parent, B, b)).toBe(true);
+  });
 });
 
 describe('11', () => {
@@ -144,5 +186,17 @@ describe('11', () => {
     expect(b.name).toBe('B');
     expect(hasOwn(child, B, b)).toBe(true);
     expect(hasOwn(parent, B, b)).toBe(false);
+  });
+
+  test('parent.get(A) should throw ERROR_TOKEN_NOT_FOUND', async () => {
+    expect(() => {
+      parent.get(A);
+    }).toThrowError(TokenNotFoundError);
+  });
+
+  test('parent.get(B) should throw ERROR_TOKEN_NOT_FOUND', async () => {
+    expect(() => {
+      parent.get(B);
+    }).toThrowError(TokenNotFoundError);
   });
 });
