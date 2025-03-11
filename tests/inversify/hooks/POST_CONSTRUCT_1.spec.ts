25b6340a6f807b8f7cf3f0a02e7d2fbc7bf59e54
kaokei <kaokei.rst@gmail.com>
2025-03-11 20:14:53 +0800

    优化postConstruct单元测试

--- tests/inversify/hooks/POST_CONSTRUCT_1.spec.ts
+++ tests/inversify/hooks/POST_CONSTRUCT_1.spec.ts
@@ -5,41 +5,228 @@ import {
   inject as Inject,
 } from 'inversify';
 
-@Injectable()
-class B {
-  @PostConstruct()
-  async init() {
-    console.log('BService postConstruct 开始');
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    console.log('BService postConstruct 完成');
+describe('POST_CONSTRUCT_1 A -> B', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService postConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService postConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
   }
-}
 
-@Injectable()
-class A {
-  constructor(@Inject(B) private bService: B) {}
+  @Injectable()
+  class A {
+    constructor(@Inject(B) public b: B) {}
 
-  @PostConstruct()
-  async init() {
-    console.log('AService postConstruct 调用时，BService 已初始化完成');
+    @PostConstruct()
+    async init() {
+      console.log('AService postConstruct 调用');
+    }
   }
-}
 
-describe('inversify activation', () => {
   let container: Container;
-  let mock: any;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
 
   beforeEach(() => {
-    mock = vi.fn().mockImplementation((_: any, av: any) => {
-      return av + '_NEW';
-    });
-
     container = new Container();
     container.bind(A).toSelf().inSingletonScope();
     container.bind(B).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
   });
 
   test('container.get(A) should work correctly', async () => {
-    const a = container.get(A);
+    await container.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
+  });
+});
+
+describe('POST_CONSTRUCT_2 B -> A', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService postConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService postConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
+  }
+
+  @Injectable()
+  class A {
+    constructor(@Inject(B) public b: B) {}
+
+    @PostConstruct()
+    async init() {
+      console.log('AService postConstruct 调用');
+    }
+  }
+
+  let container: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
+
+  beforeEach(() => {
+    container = new Container();
+    container.bind(B).toSelf().inSingletonScope();
+    container.bind(A).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
+  });
+
+  test('container.get(A) should work correctly', async () => {
+    await container.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
+  });
+});
+
+describe('POST_CONSTRUCT_3 A -> B', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService postConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService postConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
+  }
+
+  @Injectable()
+  class A {
+    @Inject(B)
+    public b!: B;
+
+    @PostConstruct()
+    async init() {
+      console.log('AService postConstruct 调用');
+    }
+  }
+
+  let container: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
+
+  beforeEach(() => {
+    container = new Container();
+    container.bind(A).toSelf().inSingletonScope();
+    container.bind(B).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
+  });
+
+  test('container.get(A) should work correctly', async () => {
+    await container.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
+  });
+});
+
+describe('POST_CONSTRUCT_4 B -> A', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService postConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService postConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
+  }
+
+  @Injectable()
+  class A {
+    @Inject(B)
+    public b!: B;
+
+    @PostConstruct()
+    async init() {
+      console.log('AService postConstruct 调用');
+    }
+  }
+
+  let container: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
+
+  beforeEach(() => {
+    container = new Container();
+    container.bind(B).toSelf().inSingletonScope();
+    container.bind(A).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
+  });
+
+  test('container.get(A) should work correctly', async () => {
+    await container.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
   });
 });
--- tests/inversify/hooks/POST_CONSTRUCT_2.spec.ts
+++ tests/inversify/hooks/POST_CONSTRUCT_2.spec.ts
@@ -5,45 +5,236 @@ import {
   inject as Inject,
 } from 'inversify';
 
-@Injectable()
-class B {
-  @PostConstruct()
-  async init() {
-    console.log('BService postConstruct 开始');
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    console.log('BService postConstruct 完成');
+describe('PRE_DESTROY_1 A -> B', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService PostConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService PostConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
   }
-}
 
-@Injectable()
-class A {
-  @Inject(B)
-  private bService!: B;
+  @Injectable()
+  class A {
+    constructor(@Inject(B) public b: B) {}
 
-  @PostConstruct()
-  async init() {
-    console.log(
-      'AService postConstruct 调用时，BService 已初始化完成',
-      this.bService
-    );
+    @PostConstruct()
+    async init() {
+      console.log('AService PostConstruct 调用');
+    }
   }
-}
 
-describe('inversify activation', () => {
-  let container: Container;
-  let mock: any;
+  let parent: Container;
+  let child: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
 
   beforeEach(() => {
-    mock = vi.fn().mockImplementation((_: any, av: any) => {
-      return av + '_NEW';
-    });
+    parent = new Container();
+    child = parent.createChild();
+    child.bind(A).toSelf().inSingletonScope();
+    parent.bind(B).toSelf().inSingletonScope();
 
-    container = new Container();
-    container.bind(A).toSelf().inSingletonScope();
-    container.bind(B).toSelf().inSingletonScope();
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
   });
 
   test('container.get(A) should work correctly', async () => {
-    const a = await container.getAsync(A);
+    await child.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
+  });
+});
+
+describe('PRE_DESTROY_2 A -> B', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService PostConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService PostConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
+  }
+
+  @Injectable()
+  class A {
+    constructor(@Inject(B) public b: B) {}
+
+    @PostConstruct()
+    async init() {
+      console.log('AService PostConstruct 调用');
+    }
+  }
+
+  let parent: Container;
+  let child: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
+
+  beforeEach(() => {
+    parent = new Container();
+    child = parent.createChild();
+    parent.bind(A).toSelf().inSingletonScope();
+    child.bind(B).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
+  });
+
+  test('container.get(A) should work correctly', async () => {
+    await child.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
+  });
+});
+
+describe('PRE_DESTROY_3 A -> B', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService PostConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService PostConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
+  }
+
+  @Injectable()
+  class A {
+    @Inject(B)
+    public b!: B;
+
+    @PostConstruct()
+    async init() {
+      console.log('AService PostConstruct 调用');
+    }
+  }
+
+  let parent: Container;
+  let child: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
+
+  beforeEach(() => {
+    parent = new Container();
+    child = parent.createChild();
+    child.bind(A).toSelf().inSingletonScope();
+    parent.bind(B).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
+  });
+
+  test('container.get(A) should work correctly', async () => {
+    await child.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
+  });
+});
+
+describe('PRE_DESTROY_4 A -> B', () => {
+  @Injectable()
+  class B {
+    beforeInit() {
+      console.log('BService PostConstruct 开始');
+    }
+
+    afterInit() {
+      console.log('BService PostConstruct 完成');
+    }
+
+    @PostConstruct()
+    async init() {
+      this.beforeInit();
+      await new Promise(resolve => setTimeout(resolve, 500));
+      this.afterInit();
+    }
+  }
+
+  @Injectable()
+  class A {
+    @Inject(B)
+    public b!: B;
+
+    @PostConstruct()
+    async init() {
+      console.log('AService PostConstruct 调用');
+    }
+  }
+
+  let parent: Container;
+  let child: Container;
+
+  let initSpyA: any;
+  let initSpyB: any;
+  let initSpyBBefore: any;
+  let initSpyBAfter: any;
+
+  beforeEach(() => {
+    parent = new Container();
+    child = parent.createChild();
+    parent.bind(A).toSelf().inSingletonScope();
+    child.bind(B).toSelf().inSingletonScope();
+
+    initSpyA = vi.spyOn(A.prototype, 'init');
+    initSpyB = vi.spyOn(B.prototype, 'init');
+    initSpyBBefore = vi.spyOn(B.prototype, 'beforeInit');
+    initSpyBAfter = vi.spyOn(B.prototype, 'afterInit');
+  });
+
+  test('container.get(A) should work correctly', async () => {
+    await child.getAsync(A);
+
+    expect(initSpyB).toHaveBeenCalledBefore(initSpyBBefore);
+    expect(initSpyBBefore).toHaveBeenCalledBefore(initSpyBAfter);
+    expect(initSpyBAfter).toHaveBeenCalledBefore(initSpyA);
   });
 });
