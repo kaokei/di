07627ac582280c7ae2249b962f1bbbbcaca73705
kaokei <kaokei.rst@gmail.com>
2026-03-29 23:16:09 +0800

    优化examples装饰器的配置

--- examples/11-lazy-inject/vite.config.ts
+++ examples/11-lazy-inject/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
+import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
   server: {
     allowedHosts: true,
   },
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { version: '2023-11' }],
+      ],
+    }),
+  ],
 });
